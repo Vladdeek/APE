cae65ef8306e387f52dd009a5ea118b9a354b986
Vladdeek <vladryazanov2709@gmail.com>
2026-07-21 10:56:43 +0300

    feat: delete all row in request student on course because f***ing student very lazy

--- frontend/src/pages/StudentCourseRequest.jsx
+++ frontend/src/pages/StudentCourseRequest.jsx
@@ -17,6 +17,8 @@ import {
 	GetEducationInstitution,
 } from '../../service/APIs/Request'
 
+const FULL_FORM_ACTIVE = false
+
 const StudentCourseRequest = () => {
 	const { courseId } = useParams()
 	const [courseInfo, setCourseInfo] = useState()
@@ -153,288 +155,297 @@ const StudentCourseRequest = () => {
 
 			<div className='grid grid-cols-3 w-full gap-3'>
 				<InputDefault
-					title='Фамилия (в именительном падеже)'
-					name='last_name_nominative'
+					title={`Фамилия ${FULL_FORM_ACTIVE ? '(в именительном падеже)' : ''}`}
+					name='last_name_nominative$'
 					value={formData.last_name_nominative}
 					onChange={handleChange}
 					validate={val => val.length >= 0}
 				/>
 				<InputDefault
-					title='Имя (в именительном падеже)'
+					title={`Имя ${FULL_FORM_ACTIVE ? '(в именительном падеже)' : ''}`}
 					name='first_name_nominative'
 					value={formData.first_name_nominative}
 					onChange={handleChange}
 					validate={val => val.length >= 0}
 				/>
 				<InputDefault
-					title='Отчество (в именительном падеже)'
+					title={`Отчество ${FULL_FORM_ACTIVE ? '(в именительном падеже)' : ''}`}
 					name='patronymic_nominative'
 					value={formData.patronymic_nominative}
 					onChange={handleChange}
 					validate={val => val.length >= 0}
 				/>
-				<InputDefault
-					title='Фамилия (в родительном падеже)'
-					description={'Иванов(-а)'}
-					name='last_name_genitive'
-					value={formData.last_name_genitive}
-					onChange={handleChange}
-					validate={val => val.length >= 0}
-					required={true}
-				/>
-				<InputDefault
-					title='Имя (в родительном падеже)'
-					description={'Иван(-а)'}
-					name='first_name_genitive'
-					value={formData.first_name_genitive}
-					onChange={handleChange}
-					validate={val => val.length >= 0}
-					required={true}
-				/>
-				<InputDefault
-					title='Отчество (в родительном падеже)'
-					description={'Иванович(-а)'}
-					name='patronymic_genitive'
-					value={formData.patronymic_genitive}
-					onChange={handleChange}
-					validate={val => val.length >= 0}
-				/>
+				{FULL_FORM_ACTIVE && (
+					<>
+						<InputDefault
+							title='Фамилия (в родительном падеже)'
+							description={'Иванов(-а)'}
+							name='last_name_genitive'
+							value={formData.last_name_genitive}
+							onChange={handleChange}
+							validate={val => val.length >= 0}
+							required={true}
+						/>
+						<InputDefault
+							title='Имя (в родительном падеже)'
+							description={'Иван(-а)'}
+							name='first_name_genitive'
+							value={formData.first_name_genitive}
+							onChange={handleChange}
+							validate={val => val.length >= 0}
+							required={true}
+						/>
+						<InputDefault
+							title='Отчество (в родительном падеже)'
+							description={'Иванович(-а)'}
+							name='patronymic_genitive'
+							value={formData.patronymic_genitive}
+							onChange={handleChange}
+							validate={val => val.length >= 0}
+						/>
+					</>
+				)}
 			</div>
+			{FULL_FORM_ACTIVE && (
+				<>
+					<div className='w-full'>
+						<InputDefault
+							title='Фамилия, указанная в дипломе о ВО или СПО (без инициалов)'
+							description={'Указать фамилию из диплома...'}
+							name='last_name_diploma'
+							value={formData.last_name_diploma}
+							onChange={handleChange}
+							validate={val => val.length >= 0}
+							required={true}
+						/>
+					</div>
 
-			<div className='w-full'>
-				<InputDefault
-					title='Фамилия, указанная в дипломе о ВО или СПО (без инициалов)'
-					description={'Указать фамилию из диплома...'}
-					name='last_name_diploma'
-					value={formData.last_name_diploma}
-					onChange={handleChange}
-					validate={val => val.length >= 0}
-					required={true}
-				/>
-			</div>
+					<div className='w-full'>
+						<FileInput
+							title='Скан свидетельства о браке / смене имени (при наличии)'
+							name='marriage_certificate_scan'
+							value={formData.marriage_certificate_scan || ''}
+							/* Явно передаем имя поля и пришедший из FileInput файл */
+							onChange={file => handleChange('marriage_certificate_scan', file)}
+							validate={val => val.length >= 0}
+							accept='application/pdf,image/jpeg,image/png, .pdf, .jpg, .jpeg, .png'
+						/>
+					</div>
 
-			<div className='w-full'>
-				<FileInput
-					title='Скан свидетельства о браке / смене имени (при наличии)'
-					name='marriage_certificate_scan'
-					value={formData.marriage_certificate_scan || ''}
-					/* Явно передаем имя поля и пришедший из FileInput файл */
-					onChange={file => handleChange('marriage_certificate_scan', file)}
-					validate={val => val.length >= 0}
-					accept='application/pdf,image/jpeg,image/png, .pdf, .jpg, .jpeg, .png'
-				/>
-			</div>
+					<div className='grid grid-cols-2 gap-3'>
+						<OptionInput
+							title={'Гражданство'}
+							required={true}
+							options={citizenship}
+							/* Передаем через handleChange */
+							onChange={obj =>
+								handleChange('citizenship', obj ? obj.value : '')
+							}
+						/>
+						<OptionInput
+							title={'Пол'}
+							required={true}
+							options={[
+								{ label: 'Мужской', value: 'Мужской' },
+								{ label: 'Женский', value: 'Женский' },
+							]}
+							onChange={obj => handleChange('gender', obj ? obj.value : '')}
+						/>
+						<DateInput
+							title='Дата рождения'
+							required
+							value={formData.birth_date}
+							onChange={val => handleChange('birth_date', val)}
+						/>
+						<MaskInput
+							title='СНИЛС'
+							mask='XXX-XXX-XXX XX'
+							required
+							onChange={val => handleChange('snils', val)}
+						/>
+					</div>
 
-			<div className='grid grid-cols-2 gap-3'>
-				<OptionInput
-					title={'Гражданство'}
-					required={true}
-					options={citizenship}
-					/* Передаем через handleChange */
-					onChange={obj => handleChange('citizenship', obj ? obj.value : '')}
-				/>
-				<OptionInput
-					title={'Пол'}
-					required={true}
-					options={[
-						{ label: 'Мужской', value: 'Мужской' },
-						{ label: 'Женский', value: 'Женский' },
-					]}
-					onChange={obj => handleChange('gender', obj ? obj.value : '')}
-				/>
-				<DateInput
-					title='Дата рождения'
-					required
-					value={formData.birth_date}
-					onChange={val => handleChange('birth_date', val)}
-				/>
-				<MaskInput
-					title='СНИЛС'
-					mask='XXX-XXX-XXX XX'
-					required
-					onChange={val => handleChange('snils', val)}
-				/>
-			</div>
+					<div className='w-full'>
+						<FileInput
+							title='Скан-копия СНИЛС'
+							description={'Допустимые форматы файлов: PDF, JPEG, PNG'}
+							required
+							name='snils_scan'
+							value={formData.snils_scan}
+							/* Явно передаем имя поля и файл */
+							onChange={file => handleChange('snils_scan', file)}
+							validate={val => val.length >= 0}
+							accept='application/pdf,image/jpeg,image/png, .pdf, .jpg, .jpeg, .png'
+						/>
+					</div>
 
-			<div className='w-full'>
-				<FileInput
-					title='Скан-копия СНИЛС'
-					description={'Допустимые форматы файлов: PDF, JPEG, PNG'}
-					required
-					name='snils_scan'
-					value={formData.snils_scan}
-					/* Явно передаем имя поля и файл */
-					onChange={file => handleChange('snils_scan', file)}
-					validate={val => val.length >= 0}
-					accept='application/pdf,image/jpeg,image/png, .pdf, .jpg, .jpeg, .png'
-				/>
-			</div>
+					<div className='grid grid-cols-3 w-full gap-3'>
+						<InputDefault
+							title='Регион места работы'
+							required
+							name='job_region'
+							value={formData.job_region}
+							onChange={handleChange}
+							validate={val => val.length >= 0}
+						/>
+						<InputDefault
+							title='Место работы (или учебное заведение для обучающихся)'
+							required
+							description={'Указать краткое или полное официальное название...'}
+							name='job_place'
+							value={formData.job_place}
+							onChange={handleChange}
+							validate={val => val.length >= 0}
+						/>
+						<InputDefault
+							title='Должность (курс обучения)'
+							required
+							name='job_title'
+							value={formData.job_title}
+							onChange={handleChange}
+							validate={val => val.length >= 0}
+						/>
+					</div>
 
-			<div className='grid grid-cols-3 w-full gap-3'>
-				<InputDefault
-					title='Регион места работы'
-					required
-					name='job_region'
-					value={formData.job_region}
-					onChange={handleChange}
-					validate={val => val.length >= 0}
-				/>
-				<InputDefault
-					title='Место работы (или учебное заведение для обучающихся)'
-					required
-					description={'Указать краткое или полное официальное название...'}
-					name='job_place'
-					value={formData.job_place}
-					onChange={handleChange}
-					validate={val => val.length >= 0}
-				/>
-				<InputDefault
-					title='Должность (курс обучения)'
-					required
-					name='job_title'
-					value={formData.job_title}
-					onChange={handleChange}
-					validate={val => val.length >= 0}
-				/>
-			</div>
+					<p className='text-xl text-[var(--black)]'>Сведения об образовании</p>
+					<div className='grid grid-cols-2 gap-3'>
+						<InputDefault
+							title='Серия диплома об образовании'
+							required
+							description={'Указать серию диплома...'}
+							name='diploma_series'
+							value={formData.diploma_series}
+							onChange={handleChange}
+							validate={val => val.length >= 0}
+						/>
+						<InputDefault
+							title='Номер диплома об образовании'
+							required
+							description={'Указать номер диплома...'}
+							name='diploma_number'
+							value={formData.diploma_number}
+							onChange={handleChange}
+							validate={val => val.length >= 0}
+						/>
+					</div>
 
-			<p className='text-xl text-[var(--black)]'>Сведения об образовании</p>
-			<div className='grid grid-cols-2 gap-3'>
-				<InputDefault
-					title='Серия диплома об образовании'
-					required
-					description={'Указать серию диплома...'}
-					name='diploma_series'
-					value={formData.diploma_series}
-					onChange={handleChange}
-					validate={val => val.length >= 0}
-				/>
-				<InputDefault
-					title='Номер диплома об образовании'
-					required
-					description={'Указать номер диплома...'}
-					name='diploma_number'
-					value={formData.diploma_number}
-					onChange={handleChange}
-					validate={val => val.length >= 0}
-				/>
-			</div>
+					<div className='w-full'>
+						<FileInput
+							title='Скан-копия диплома об образовании'
+							required
+							name='diploma_scan'
+							value={formData.diploma_scan}
+							/* Явно передаем имя поля и файл */
+							onChange={file => {
+								console.log(file)
+								handleChange('diploma_scan', file)
+							}}
+							validate={val => val.length >= 0}
+							accept='application/pdf,image/jpeg,image/png, .pdf, .jpg, .jpeg, .png'
+						/>
+					</div>
 
-			<div className='w-full'>
-				<FileInput
-					title='Скан-копия диплома об образовании'
-					required
-					name='diploma_scan'
-					value={formData.diploma_scan}
-					/* Явно передаем имя поля и файл */
-					onChange={file => {
-						console.log(file)
-						handleChange('diploma_scan', file)
-					}}
-					validate={val => val.length >= 0}
-					accept='application/pdf,image/jpeg,image/png, .pdf, .jpg, .jpeg, .png'
-				/>
-			</div>
+					<div className='grid grid-cols-2 gap-3'>
+						<OptionInput
+							title={'Уровень образования'}
+							required={true}
+							options={[
+								{ label: 'Бакалавриат', value: 'Бакалавриат' },
+								{ label: 'Специалитет', value: 'Специалитет' },
+								{ label: 'Магистратура', value: 'Магистратура' },
+								{ label: 'Аспирантура', value: 'Аспирантура' },
+							]}
+							onChange={obj =>
+								handleChange('education_level', obj ? obj.value : '')
+							}
+						/>
+						<InputDefault
+							title='Квалификация по диплому'
+							required
+							name='diploma_qualification'
+							value={formData.diploma_qualification}
+							onChange={handleChange}
+							validate={val => val.length >= 0}
+						/>
+						<OptionInput
+							title={'Полное наименование учебного заведения'}
+							required={true}
+							options={educationInstitution}
+							onChange={obj =>
+								handleChange('education_institution_name', obj ? obj.value : '')
+							}
+						/>
+						<InputDefault
+							title='Если Вашего учебного заведения нет в списке, укажите его название здесь:'
+							name='education_institution_name_missed'
+							value={formData.education_institution_name_missed || ''}
+							onChange={handleChange}
+							validate={val => val.length >= 0}
+						/>
+					</div>
 
-			<div className='grid grid-cols-2 gap-3'>
-				<OptionInput
-					title={'Уровень образования'}
-					required={true}
-					options={[
-						{ label: 'Бакалавриат', value: 'Бакалавриат' },
-						{ label: 'Специалитет', value: 'Специалитет' },
-						{ label: 'Магистратура', value: 'Магистратура' },
-						{ label: 'Аспирантура', value: 'Аспирантура' },
-					]}
-					onChange={obj =>
-						handleChange('education_level', obj ? obj.value : '')
-					}
-				/>
-				<InputDefault
-					title='Квалификация по диплому'
-					required
-					name='diploma_qualification'
-					value={formData.diploma_qualification}
-					onChange={handleChange}
-					validate={val => val.length >= 0}
-				/>
-				<OptionInput
-					title={'Полное наименование учебного заведения'}
-					required={true}
-					options={educationInstitution}
-					onChange={obj =>
-						handleChange('education_institution_name', obj ? obj.value : '')
-					}
-				/>
-				<InputDefault
-					title='Если Вашего учебного заведения нет в списке, укажите его название здесь:'
-					name='education_institution_name_missed'
-					value={formData.education_institution_name_missed || ''}
-					onChange={handleChange}
-					validate={val => val.length >= 0}
-				/>
-			</div>
+					<p className='text-xl text-[var(--black)]'>Удостоверение личности</p>
+					<div className='grid grid-cols-2 gap-3'>
+						<InputDefault
+							title='Тип документа, удостоверяющего личность'
+							required
+							name='identity_document_type'
+							value={formData.identity_document_type}
+							onChange={handleChange}
+							validate={val => val.length >= 0}
+						/>
+						<MaskInput
+							title='Серия и номер документа'
+							mask='XXXX XXXXXX'
+							required
+							onChange={val => handleChange('number_and_series', val)}
+						/>
+						<InputDefault
+							title='Кем выдан документ'
+							required
+							name='issued_by'
+							value={formData.issued_by}
+							onChange={handleChange}
+							validate={val => val.length >= 0}
+						/>
+						<DateInput
+							title='Дата выдачи документа'
+							required
+							value={formData.issue_date}
+							onChange={val => handleChange('issue_date', val)}
+						/>
+					</div>
 
-			<p className='text-xl text-[var(--black)]'>Удостоверение личности</p>
-			<div className='grid grid-cols-2 gap-3'>
-				<InputDefault
-					title='Тип документа, удостоверяющего личность'
-					required
-					name='identity_document_type'
-					value={formData.identity_document_type}
-					onChange={handleChange}
-					validate={val => val.length >= 0}
-				/>
-				<MaskInput
-					title='Серия и номер документа'
-					mask='XXXX XXXXXX'
-					required
-					onChange={val => handleChange('number_and_series', val)}
-				/>
-				<InputDefault
-					title='Кем выдан документ'
-					required
-					name='issued_by'
-					value={formData.issued_by}
-					onChange={handleChange}
-					validate={val => val.length >= 0}
-				/>
-				<DateInput
-					title='Дата выдачи документа'
-					required
-					value={formData.issue_date}
-					onChange={val => handleChange('issue_date', val)}
-				/>
-			</div>
+					<div className='w-full'>
+						<InputDefault
+							title='Адрес регистрации'
+							required
+							name='registration_address'
+							value={formData.registration_address}
+							onChange={handleChange}
+							validate={val => val.length >= 0}
+						/>
+					</div>
 
-			<div className='w-full'>
-				<InputDefault
-					title='Адрес регистрации'
-					required
-					name='registration_address'
-					value={formData.registration_address}
-					onChange={handleChange}
-					validate={val => val.length >= 0}
-				/>
-			</div>
-
-			<p className='text-xl text-[var(--black)]'>Контакты</p>
-			<div className='grid grid-cols-2 gap-3'>
-				<MaskInput
-					title='Телефон'
-					mask='+X (XXX) XXX-XX-XX'
-					required
-					onChange={val => handleChange('phone', val)}
-				/>
-				<InputDefault
-					title='Действующий адрес электронной почты (e-mail)'
-					required
-					name='email'
-					value={formData.email}
-					onChange={handleChange}
-					validate={val => val.length >= 0}
-				/>
-			</div>
+					<p className='text-xl text-[var(--black)]'>Контакты</p>
+					<div className='grid grid-cols-2 gap-3'>
+						<MaskInput
+							title='Телефон'
+							mask='+X (XXX) XXX-XX-XX'
+							required
+							onChange={val => handleChange('phone', val)}
+						/>
+						<InputDefault
+							title='Действующий адрес электронной почты (e-mail)'
+							required
+							name='email'
+							value={formData.email}
+							onChange={handleChange}
+							validate={val => val.length >= 0}
+						/>
+					</div>
+				</>
+			)}
 
 			<div className='flex gap-3 w-full items-center justify-start'>
 				<button
